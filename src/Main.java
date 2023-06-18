9b4e7ba45fdfa2a5502c443f801d90cce7888cb1
Hoang Nguyen <hoangmegamind1302@gmail.com>
2023-06-18 21:15:42 +0700

    :3

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/*
+Nguyễn Huy Hoàng
+Lê Quang Trung
+Final Project BE1 - Movie Ticket Booking System Online with Java
+ */
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Movie Ticket Booking System Online with Java");
+    }
+}
