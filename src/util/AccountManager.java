bb6cccabb0936038fe315d5a5c9254e8f1172077
Hoang Nguyen <hoangmegamind1302@gmail.com>
2023-06-27 15:23:25 +0700

    account Manager updated

--- src/util/AccountManager.java
+++ src/util/AccountManager.java
@@ -1,11 +1,6 @@
 package util;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,10 +15,20 @@ public class AccountManager extends HashMap<String, User> {
 
     private Map<String, User> loadUsersFromFile() {
         Map<String, User> loadedUsers = new HashMap<>();
-        try (FileInputStream fis = new FileInputStream(fileName);
-             ObjectInputStream ois = new ObjectInputStream(fis)) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                if (data.length == 4) {
+                    String userID = data[0];
+                    String userName = data[1];
+                    String password = data[2];
+                    int fund = Integer.parseInt(data[3]);
 
-            loadedUsers = (Map<String, User>) ois.readObject();
+                    User user = new User(userID, userName, password, fund);
+                    loadedUsers.put(userName, user);
+                }
+            }
         } catch (FileNotFoundException e) {
             // File does not exist, return an empty map
         } catch (IOException | ClassNotFoundException e) {
@@ -33,15 +38,18 @@ public class AccountManager extends HashMap<String, User> {
     }
 
     private void saveUsersToFile() {
-        try (FileOutputStream fos = new FileOutputStream(fileName);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-
-            oos.writeObject(this);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            for (User user : this.values()) {
+                String line = user.getUserID() + "," + user.getUserName() + "," + user.getPassword() + ","
+                        + user.getFund();
+                bw.write(line);
+                bw.newLine();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     public void addNewUser(User newUser) {
         if (isUserExists(newUser.getUserName())) {
             System.out.println("Username already exists. Please choose a different username.");
