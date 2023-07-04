8f07a3a6c75f491435708475b63c7eab202c4e58
Hoang Nguyen <hoangmegamind1302@gmail.com>
2023-07-04 21:09:39 +0700

    xoa Role

--- src/user_roles/Admin.java
+++ src/user_roles/Admin.java
@@ -4,7 +4,7 @@ import util.FilmManager;
 
 public class Admin  extends User {
 
-    public Admin(String userName, String password, int fund, int role) {
-        super(userName, password, fund, role);
+    public Admin(String userName, String password, int fund) {
+        super(userName, password, fund);
     }
 }
